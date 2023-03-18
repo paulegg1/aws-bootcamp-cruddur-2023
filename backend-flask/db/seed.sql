a851ca8648783c767add2d545622cbce4fdd15f3
paulegg1 <paulegg+github@gmail.com>
2023-03-18 21:12:46 +0000

    needed some changes to the schema to make the lambda trigger work

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (email, display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('andrew@exampro.co', 'Andrew Brown', 'andrewbrown' ,'7659c102-7ad4-44bd-bdc3-208f934d1f41'),
+  ('bayko@exampro.co', 'Andrew Bayko', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
