11b62be9bf628314f786a362aa2c3e7ccea3f129
paulegg1 <67067195+paulegg1@users.noreply.github.com>
2023-03-20 18:56:27 +0000

    Update home_activities.py

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,38 +1,20 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool, query_wrap_array
-import logging
+from lib.db import db
 
-# get root logger
-home_logger = logging.getLogger('app') 
-
-tracer = trace.get_tracer("home.activities")
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
-    home_logger.info('message from INSIDE home activities module')
-    with tracer.start_as_current_span("home-activites-mock-data"):
-      now = datetime.now(timezone.utc).astimezone()
-      span = trace.get_current_span()
-      span.set_attribute("user.id", "andrewbrown")
-      span.set_attribute("app.now", now.isoformat())
-
-      sql = query_wrap_array("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-      """)
+    #logger.info("HomeActivities")
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
       print(sql)
 
       with pool.connection() as conn:
