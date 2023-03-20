75321087b8665d86a37aa91a5927a01478cdd41e
paulegg1 <67067195+paulegg1@users.noreply.github.com>
2023-03-20 19:00:29 +0000

    Update home_activities.py

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,30 +2,19 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
 from lib.db import db
+import logging
 
-#tracer = trace.get_tracer("home.activities")
+# get root logger
+home_logger = logging.getLogger('app') 
+
+tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
-    #logger.info("HomeActivities")
+    home_logger.info('message from INSIDE home activities module')
     #with tracer.start_as_current_span("home-activites-mock-data"):
-    #  span = trace.get_current_span()
-    #  now = datetime.now(timezone.utc).astimezone()
-    #  span.set_attribute("app.now", now.isoformat())
+ 
     sql = db.template('activities','home')
     results = db.query_array_json(sql)
+    span.set_attribute("app.result_length", len(results))
     return results
-      print(sql)
-
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-          home_logger.info("======= NOT JSON =====")
-          home_logger.info(json)    
-      return json[0] 
-
-      span.set_attribute("app.result_length", len(results))
-      return results
