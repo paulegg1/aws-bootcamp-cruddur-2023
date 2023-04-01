0c286a99995a9e5f624f21a412bb9a4e7e71824d
paulegg1 <paulegg+github@gmail.com>
2023-04-01 12:24:18 +0000

    cognito-update-users

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,8 @@
 INSERT INTO public.users (email, display_name, handle, cognito_user_id)
 VALUES
   ('andrew@exampro.co', 'Andrew Brown', 'andrewbrown' ,'7659c102-7ad4-44bd-bdc3-208f934d1f41'),
-  ('bayko@exampro.co', 'Andrew Bayko', 'bayko' ,'MOCK');
+  ('bayko@exampro.co', 'Andrew Bayko', 'bayko' ,'MOCK'),
+  ('paul@eggitservices.com', 'Paul', 'Paulegg' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
