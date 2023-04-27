5eb7a61b239964e17077c89de27291449dd3c49a
paulegg1 <paulegg+github@gmail.com>
2023-04-27 16:03:07 +0000

    End of the live stream vid.

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -2,8 +2,10 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
-import { LambdaApplication } from 'aws-cdk-lib/aws-codedeploy';
+import * as dotenv from 'dotenv';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+dotenv.config();
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,8 +16,10 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     
     const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
     const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    const folderInput: string = process.env.THUMBING_FOLDER_INPUT as string;
+    const folderOutput: string = process.env.THUMBING_FOLDER_OUTPUT as string;
     const bucket = this.createBucket(bucketName);
-    const lambda = this.createLambda(functionPath);
+    const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
 
     // REMOVE THIS:
     // example resource
@@ -34,11 +38,19 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
   }
 
 
-  createLambda(functionPath: string): lambda.IFunction {
+  createLambda(functionPath: string, bucketName: string, folderInput: string, folderOutput: string): lambda.IFunction {
     const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
       code: lambda.Code.fromAsset(functionPath),
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_18_X,
+      environment: {
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderInput,
+        FOLDER_OUTPUT: folderOutput,
+        PROCESS_WIDTH: '512',
+        PROCESS_HEIGHT: '512',
+
+      }
     });
     return lambdaFunction; 
   }
